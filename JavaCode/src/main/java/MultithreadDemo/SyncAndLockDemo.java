c515ae3aa9f2977eb14bd15feb39949407152391
LuckyShawn <LuckyShawn@foxmail.com>
2019-05-21 17:56:59 +0800

    SyncAndLockDemo

--- JavaCode/src/main/java/MultithreadDemo/SyncAndLockDemo.java
+++ JavaCode/src/main/java/MultithreadDemo/SyncAndLockDemo.java
@@ -6,11 +6,11 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Description synchronized和lock区别
- * 题目：多线程之间按顺序调用，实现A->B->C三个线程启动：
+ * 题目：多线程之间按顺序调用， 实现A->B->C三个线程启动：
  *                  AA打印5次，BB打印10次，CC打印15次
  *                  AA打印5次，BB打印10次，CC打印15次
  *                  AA打印5次，BB打印10次，CC打印15次
- *                  ......
+ *                  .........
  *                  10次
  * @Author shawn
  * @create 2019/5/15 0015
